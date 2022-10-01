4ebe08f6de291f639a1e5e58ef814c091f8db561
Jossie Esteban Rodriguez Salazar <jerodriguez@merqueo.com>
2022-10-01 18:58:36 -0500

    feat(editors): adds news telescope and lsp nvim keymaps

--- editors/nvim/after/plugin/lsp.lua
+++ editors/nvim/after/plugin/lsp.lua
@@ -34,14 +34,10 @@ local on_attach = function(_, bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
 end
 
--- editors/nvim/after/plugin/telescope.lua
+++ editors/nvim/after/plugin/telescope.lua
@@ -23,14 +23,21 @@ telescope.setup({
 vim.keymap.set('n', '<leader>ff', require 'telescope.builtin'.git_files, { remap = false })
 vim.keymap.set('n', '<leader>fa', require 'telescope.builtin'.find_files, { remap = false })
 vim.keymap.set('n', '<leader>fs', require 'telescope.builtin'.live_grep, { remap = false })
+vim.keymap.set('n', '<leader>fq', require 'telescope.builtin'.current_buffer_fuzzy_find, { remap = false })
 vim.keymap.set('n', '<leader>fb', require 'telescope.builtin'.buffers, { remap = false })
 vim.keymap.set('n', '<leader>fh', require 'telescope.builtin'.help_tags, { remap = false })
+vim.keymap.set('n', '<leader>fhk', require 'telescope.builtin'.keymaps, { remap = false })
 vim.keymap.set('n', '<leader>fgb', require 'telescope.builtin'.git_branches, { remap = false })
+vim.keymap.set('n', '<leader>fgs', require 'telescope.builtin'.git_status, { remap = false })
 vim.keymap.set('n', '<leader>fc', require 'telescope.builtin'.command_history, { remap = false })
+vim.keymap.set('n', '<leader>fy', require 'telescope.builtin'.lsp_document_symbols, { remap = false })
+vim.keymap.set('n', '<leader>fx', require 'telescope.builtin'.treesitter, { remap = false })
+vim.keymap.set('n', '<leader>fd', require 'telescope.builtin'.diagnostics, { remap = false })
 
--- vim.keymap.set('n', '<leader>fy', require 'telescope.builtin'.treesitter, { remap = false })
--- vim.keymap.set('n', '<leader>gd', require 'telescope.builtin'.lsp_definitions, { remap = false })
--- vim.keymap.set('n', '<leader>gr', require 'telescope.builtin'.lsp_references, { remap = false })
+vim.keymap.set('n', 'gd', require 'telescope.builtin'.lsp_definitions, { remap = false })
+vim.keymap.set('n', 'gr', require 'telescope.builtin'.lsp_references, { remap = false })
+vim.keymap.set('n', 'gi', require 'telescope.builtin'.lsp_implementations, { remap = false })
+vim.keymap.set('n', 'gtd', require 'telescope.builtin'.lsp_type_definitions, { remap = false })
 
 vim.api.nvim_set_keymap(
     "n",
