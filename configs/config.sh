31c65f9ba8151a95656d7ae804d6592537bbb2cc
Jossie Esteban Rodriguez Salazar <jestebanrods@gmail.com>
2023-07-27 09:06:00 -0500

    feat: reverts configs file

--- configs/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-KERNEL_NAME=$(uname -s)
-DOTFILES_PATH=$HOME/Documents/code/jestebanrods/repos/dotfiles
-IS_MAC=$(uname -s | grep -qi "Darwin" && echo true || echo false)
-
-export DOTFILES_PATH
--- lib.sh
+++ lib.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-source configs/config.sh
-# export DOTFILES_PATH=$HOME/Documents/code/jestebanrods/repos/dotfiles
+KERNEL_NAME=$(uname -s)
+DOTFILES_PATH=$HOME/Documents/code/jestebanrods/repos/dotfiles
+IS_MAC=$(uname -s | grep -qi "Darwin" && echo true || echo false)
+
+export DOTFILES_PATH
+
 libs_folder="$DOTFILES_PATH/lib"
 
 # Comprobamos si la carpeta "libs" existe
